e5a3923eea6f993fdcdedbe99c7458f12c7af8d2
TrueMB <ich882777@gmail.com>
2022-01-02 19:42:09 +0100

    Update Config

--- src/me/truemb/rentit/main/Main.java
+++ src/me/truemb/rentit/main/Main.java
@@ -125,7 +125,7 @@ public class Main extends JavaPlugin {
 	//NAMESPACES
 	public NamespacedKey guiItem = new NamespacedKey(this, "guiItem");
 
-	private static final int configVersion = 8;
+	private static final int configVersion = 9;
     private static final int SPIGOT_RESOURCE_ID = 90195;
     private static final int BSTATS_PLUGIN_ID = 12060;
     
