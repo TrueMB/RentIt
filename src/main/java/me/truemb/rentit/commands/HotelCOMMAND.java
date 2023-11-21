99048d289e64523e09b01cbc23dda022e172c162
TrueMB <->
2023-11-21 14:40:34 +0100

    Add: Command /hotel delete <ID>

--- src/main/java/me/truemb/rentit/commands/HotelCOMMAND.java
+++ src/main/java/me/truemb/rentit/commands/HotelCOMMAND.java
@@ -205,28 +205,10 @@ public class HotelCOMMAND extends BukkitCommand {
 				}
 
 				RentTypeHandler rentHandler = this.instance.getMethodes().getTypeHandler(this.type, hotelId);
-				CategoryHandler catHandler = this.instance.getMethodes().getCategory(this.type, rentHandler.getCatID());
 				
-			    String alias = rentHandler != null && rentHandler.getAlias() != null ? rentHandler.getAlias() : String.valueOf(hotelId);
-			    String catAlias = catHandler != null && catHandler.getAlias() != null ? catHandler.getAlias() : String.valueOf(catHandler.getCatID());
-			    
-				BlockVector3 min = this.instance.getAreaFileManager().getMinBlockpoint(this.type, hotelId);
-				BlockVector3 max = this.instance.getAreaFileManager().getMaxBlockpoint(this.type, hotelId);
-				this.instance.getBackupManager().paste(this.type, hotelId, min, max, p.getWorld(), false);
-				this.instance.getBackupManager().deleteSchem(this.type, hotelId);
-				this.instance.getMethodes().deleteType(this.type, hotelId);
-				this.instance.getMethodes().deleteArea(p, this.type, hotelId);
-				this.instance.getMethodes().deleteSigns(this.type, hotelId);
-				this.instance.getMethodes().clearPlayersFromRegion(this.type, hotelId, p.getWorld());
-
-				this.instance.getAreaFileManager().unsetDoorClosed(this.type, hotelId);
-				this.instance.getDoorFileManager().clearDoors(this.type, hotelId);
-
-				p.sendMessage(this.instance.getMessage("hotelDeleted")
-						.replaceAll("(?i)%" + "hotelId" + "%", String.valueOf(hotelId))
-						.replaceAll("(?i)%" + "catAlias" + "%", catAlias)
-						.replaceAll("(?i)%" + "alias" + "%", alias));
+				this.deleteHotel(p, rentHandler);
 				return true;
+				
 			} else if (args[0].equalsIgnoreCase("permissions")) {
 				
 				if(!this.instance.getMethodes().isSubCommandEnabled("hotel", "permissions")) {
@@ -514,6 +496,35 @@ public class HotelCOMMAND extends BukkitCommand {
 						}
 					}
 				});
+				return true;
+
+			}  else if (args[0].equalsIgnoreCase("delete")) {
+				
+				if(!this.instance.getMethodes().isSubCommandEnabled("hotel", "delete")) {
+					sender.sendMessage(this.instance.getMessage("commandDisabled"));
+					return true;
+				}
+
+				if (!this.instance.getMethodes().hasPermissionForCommand(p, true, "hotel", "delete")) {
+					p.sendMessage(this.instance.getMessage("perm"));
+					return true;
+				}
+
+				if (!args[1].matches("[0-9]+")) {
+					p.sendMessage(this.instance.getMessage("notANumber"));
+					return true;
+				}
+
+				int hotelId = Integer.parseInt(args[1]);
+				RentTypeHandler rentHandler = this.instance.getMethodes().getTypeHandler(this.type, hotelId);
+				
+				if (hotelId < 0) {
+					// PLAYER NOT IN SHOP AREA, CANT FIND ID
+					p.sendMessage(this.instance.getMessage("hotelIdNotValid"));
+					return true;
+				}
+				
+				this.deleteHotel(p, rentHandler);
 				return true;
 
 			} else if (args[0].equalsIgnoreCase("catInfo")) {
@@ -1257,6 +1268,34 @@ public class HotelCOMMAND extends BukkitCommand {
 			this.sendHelp(p, "hotelUserHelp");
 
 		return true;
+	}
+
+	private void deleteHotel(Player p, RentTypeHandler rentHandler) {
+
+		int hotelId = rentHandler.getID();
+		
+		CategoryHandler catHandler = this.instance.getMethodes().getCategory(this.type, rentHandler.getCatID());
+		
+	    String alias = rentHandler != null && rentHandler.getAlias() != null ? rentHandler.getAlias() : String.valueOf(hotelId);
+	    String catAlias = catHandler != null && catHandler.getAlias() != null ? catHandler.getAlias() : String.valueOf(catHandler.getCatID());
+	    
+		BlockVector3 min = this.instance.getAreaFileManager().getMinBlockpoint(this.type, hotelId);
+		BlockVector3 max = this.instance.getAreaFileManager().getMaxBlockpoint(this.type, hotelId);
+		this.instance.getBackupManager().paste(this.type, hotelId, min, max, p.getWorld(), false);
+		this.instance.getBackupManager().deleteSchem(this.type, hotelId);
+		this.instance.getMethodes().deleteType(this.type, hotelId);
+		this.instance.getMethodes().deleteArea(p, this.type, hotelId);
+		this.instance.getMethodes().deleteSigns(this.type, hotelId);
+		this.instance.getMethodes().clearPlayersFromRegion(this.type, hotelId, p.getWorld());
+
+		this.instance.getAreaFileManager().unsetDoorClosed(this.type, hotelId);
+		this.instance.getDoorFileManager().clearDoors(this.type, hotelId);
+
+		p.sendMessage(this.instance.getMessage("hotelDeleted")
+				.replaceAll("(?i)%" + "hotelId" + "%", String.valueOf(hotelId))
+				.replaceAll("(?i)%" + "catAlias" + "%", catAlias)
+				.replaceAll("(?i)%" + "alias" + "%", alias));
+		
 	}
 
 	private boolean sendCategoryInfo(Player p, int catId) {
